82dda4d8398d63e58c0455797b45c0394a21b3cf
Carlos Velasco <carlos@2activedesign.com>
2017-06-04 08:57:45 +0200

    Working version

--- js/master.js
+++ js/master.js
@@ -4,44 +4,48 @@ window.lemonade = {
   signCost: 0.15,
   allTimeProfit: 0,
   dailyProfit: 0,
-  cups: document.getElementById('cups')
-    .value,
-  price: document.getElementById('price')
-    .value,
-  signs: document.getElementById('signs')
-    .value,
-  randomNum: Math.random,
-  weatherPool: ['Sunny', 'Cloudy', 'Hot and Dry'],
-  weatherToday: false,
-
-  play: function play() {
-    const profits = lemonade.cups * lemonade.price;
-    const sold = lemonade.signs * lemonade.cups * lemonade.randomNum;
-    const expenses = (lemonade.signs * lemonade.signCost) + (lemonade.cups * lemonade.cupCost);
-    lemonade.dailyProfit = (profits - expenses);
-    lemonade.allTimeProfit += lemonade.dailyProfit;
-
-    Materialize.toast('Day: ' + lemonade.day, 5000);
-    Materialize.toast('Cups Sold: ' + sold, 5000);
-    Materialize.toast('Expenses: $' + expenses, 5000);
-    Materialize.toast('Profits: $' + profits, 5000);
-    Materialize.toast('All-time Profit: $' + lemonade.allTimeProfit, 5000);
-    lemonade.dailyProfit = 0;
-    lemonade.day++;
-    document.getElementById('cups')
-      .value = 75;
-    document.getElementById('signs')
-      .value = 4;
-    document.getElementById('price')
-      .value = 0.1;
-  }
+  randomNum: function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  weather: ['Cloudy', 'Sunny', 'Hot and Dry']
 };
 
-document.getElementById('cups')
-  .addEventListener('click', lemonade.cups);
-document.getElementById('signs')
-  .addEventListener('click', lemonade.signs);
-document.getElementById('price')
-  .addEventListener('click', lemonade.price);
+let tomorrowWeatherVariant = 3;
+let tomorrowForecast = lemonade.weather[tomorrowWeatherVariant];
+let todayWeatherVariant = tomorrowWeatherVariant;
+let todayForecast = tomorrowForecast;
+
+function nextDay() {
+  const cups = document.getElementById('cups')
+    .value;
+  const price = document.getElementById('price')
+    .value;
+  const signs = document.getElementById('signs')
+    .value;
+  if (lemonade.day < 2) {
+
+  }
+  todayWeatherVariant = tomorrowWeatherVariant;
+  todayForecast = tomorrowForecast;
+
+  const marketing = Math.round((signs / price) * ((todayWeatherVariant / 1.5) + 1));
+  let sold = marketing;
+  if (marketing > cups) { sold = cups; }
+  const profits = Math.round((sold * price) * 100) / 100;
+  const expenses = Math.round(((signs * lemonade.signCost) + (cups * lemonade.cupCost)) * 100) / 100;
+  lemonade.dailyProfit = Math.round((profits - expenses) * 100) / 100;
+  lemonade.allTimeProfit = (Math.round(lemonade.allTimeProfit * 100) / 100) + lemonade.dailyProfit;
+  Materialize.toast('Day: ' + lemonade.day, 5000);
+  Materialize.toast('Cups Sold: ' + sold, 5000);
+  Materialize.toast('Expenses: $' + expenses, 5000);
+  Materialize.toast('Profits: $' + profits, 5000);
+  Materialize.toast('All-time Profit: $' + lemonade.allTimeProfit, 5000);
+  tomorrowWeatherVariant = lemonade.randomNum(0, 3);
+  tomorrowForecast = lemonade.weather[tomorrowWeatherVariant];
+  Materialize.toast('Forecast: ' + tomorrowForecast, 5000);
+  lemonade.dailyProfit = 0;
+  lemonade.day++;
+}
+
 document.getElementById('play')
-  .addEventListener('click', lemonade.play);
+  .addEventListener('click', nextDay);
