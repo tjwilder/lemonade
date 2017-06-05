fec888d44b45c3d8b1d494e3d81cd3c9d06e5d59
Carlos Velasco <carlos@2activedesign.com>
2017-06-05 22:15:27 +0200

    revamp design

--- js/master.js
+++ js/master.js
@@ -7,11 +7,11 @@ window.lemonade = {
   randomNumber: function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
-  weather: ['Cloudy', 'Sunny', 'Hot & Dry'],
+  weather: ['Cloudy', 'Sunny', 'Hot/Dry'],
   tomorrowWeatherVariant: 3,
-  tomorrowForecast: 'Hot & Dry',
+  tomorrowForecast: 'Hot/Dry',
   todayWeatherVariant: 3,
-  todayForecast: 'Hot & Dry',
+  todayForecast: 'Hot/Dry',
   nextDay: function nextDay() {
     const cups = document.getElementById('cups')
       .value;
@@ -21,7 +21,7 @@ window.lemonade = {
       .value;
     if (lemonade.day < 2) {
       lemonade.todayWeatherVariant = 3;
-      lemonade.todayForecast = 'Hot & Dry';
+      lemonade.todayForecast = 'Hot/Dry';
     } else {
       lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
       lemonade.todayForecast = lemonade.tomorrowForecast;
@@ -31,10 +31,10 @@ window.lemonade = {
     if (marketingResult > cups) { sold = cups; }
     const profits = Math.round((sold * price) * 100) / 100;
     const expenses = Math.round(((signs * lemonade.signCost) + (cups * lemonade.cupCost)) * 100) / 100;
-    lemonade.dailyProfit = (profits - expenses);
+    lemonade.dailyProfit = Math.round((profits - expenses) * 100) / 100;
     lemonade.allTimeProfit += lemonade.dailyProfit;
     lemonade.allTimeProfit = Math.round(lemonade.allTimeProfit * 100) / 100;
-    Materialize.toast('Profit: $' + profits + ' | Expense: $' + expenses + ' | Net: $' + lemonade.dailyProfit, 5000);
+    Materialize.toast('Profit: $' + profits + ' | Expense: $' + expenses + ' | Net: $' + lemonade.dailyProfit, 6000);
 
     lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 3);
     lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
@@ -45,11 +45,16 @@ window.lemonade = {
     document.getElementById('weatherDisplay')
       .innerText = lemonade.tomorrowForecast;
     document.getElementById('cupsSoldDisplay')
-      .innerText = 'Sold: ' + sold;
+      .innerText = sold;
     document.getElementById('grandTotalDisplay')
       .innerText = '$' + lemonade.allTimeProfit;
   },
 };
+const play = document.getElementById('play');
 
-document.getElementById('play')
-  .addEventListener('click', lemonade.nextDay);
+play.addEventListener('click', lemonade.nextDay);
+
+window.onload = function () {
+  play.className += ' scale-in';
+  Materialize.fadeInImage('#lemonadeImage');
+};
