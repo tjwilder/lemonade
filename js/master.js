50e73e35c7bd773f98095a2a142252764c07b2e6
Carlos Velasco <carlos@2activedesign.com>
2017-06-06 04:38:45 +0200

    animated SVG

--- js/master.js
+++ js/master.js
@@ -2,16 +2,18 @@ window.lemonade = {
   day: 1,
   cupCost: 0.05,
   signCost: 0.15,
-  allTimeProfit: 0,
+  allTimeProfit: 10,
   dailyProfit: 0,
+  bar: document.getElementById('percentageBar'),
   randomNumber: function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
-  weather: ['Cloudy', 'Sunny', 'Hot/Dry'],
+  weather: ['mdi mdi-weather-lightning-rainy mdi-36px', 'mdi mdi-weather-cloudy mdi-36px', 'mdi mdi-weather-partlycloudy mdi-36px', 'mdi mdi-weather-sunny mdi-36px'],
   tomorrowWeatherVariant: 3,
-  tomorrowForecast: 'Hot/Dry',
+  tomorrowForecast: '',
   todayWeatherVariant: 3,
-  todayForecast: 'Hot/Dry',
+  todayForecast: '',
+  emotionBank: ['mdi mdi-emoticon-dead mdi-36px', 'mdi mdi-emoticon-neutral mdi-36px', 'mdi mdi-emoticon mdi-36px', 'mdi mdi-emoticon-excited mdi-36px'],
   nextDay: function nextDay() {
     const cups = document.getElementById('cups')
       .value;
@@ -19,6 +21,8 @@ window.lemonade = {
       .value;
     const signs = document.getElementById('signs')
       .value;
+    const forecastDisplay = document.getElementById('forecastDisplay');
+    const emotion = document.getElementById('emotionDisplay');
     if (lemonade.day < 2) {
       lemonade.todayWeatherVariant = 3;
       lemonade.todayForecast = 'Hot/Dry';
@@ -26,26 +30,40 @@ window.lemonade = {
       lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
       lemonade.todayForecast = lemonade.tomorrowForecast;
     }
-    const marketingResult = (((signs / 1.2) + (lemonade.randomNumber(1, 5) / 4.8)) / price) * ((lemonade.todayWeatherVariant / 2.8) + 0.5);
+    const marketingResult = (((signs * 1.35) + (lemonade.randomNumber(14.4, 94.2) / 70)) / price) * ((lemonade.todayWeatherVariant / 2.5) + 0.08);
     let sold = Math.round(marketingResult);
     if (marketingResult > cups) { sold = cups; }
+    if (sold > 0) {
+      lemonade.bar.style.width = ((sold / cups) * 100) + '%';
+    } else {
+      lemonade.bar.style.width = 0;
+    }
     const profits = Math.round((sold * price) * 100) / 100;
     const expenses = Math.round(((signs * lemonade.signCost) + (cups * lemonade.cupCost)) * 100) / 100;
     lemonade.dailyProfit = Math.round((profits - expenses) * 100) / 100;
     lemonade.allTimeProfit += lemonade.dailyProfit;
     lemonade.allTimeProfit = Math.round(lemonade.allTimeProfit * 100) / 100;
     Materialize.toast('Profit: $' + profits + ' | Expense: $' + expenses + ' | Net: $' + lemonade.dailyProfit, 6000);
+    if (lemonade.allTimeProfit < 0.01) {
+      emotion.className = lemonade.emotionBank[0];
+    } else if (lemonade.allTimeProfit < 10) {
+      emotion.className = lemonade.emotionBank[1];
+    } else if (lemonade.allTimeProfit < 25) {
+      emotion.className = lemonade.emotionBank[2];
+    } else if (lemonade.allTimeProfit > 49) {
+      emotion.className = lemonade.emotionBank[3];
+    }
 
-    lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 3);
+    lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 4);
+    // lemonade.tomorrowWeatherVariant = 1;
     lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
+    forecastDisplay.className = lemonade.tomorrowForecast;
 
     lemonade.day++;
     document.getElementById('dayDisplay')
       .innerText = 'Day ' + lemonade.day;
-    document.getElementById('weatherDisplay')
-      .innerText = lemonade.tomorrowForecast;
     document.getElementById('cupsSoldDisplay')
-      .innerText = sold;
+      .innerText = 'Cups Sold Today: ' + sold;
     document.getElementById('grandTotalDisplay')
       .innerText = '$' + lemonade.allTimeProfit;
   },
@@ -58,3 +76,4 @@ window.onload = function () {
   play.className += ' scale-in';
   Materialize.fadeInImage('#lemonadeImage');
 };
+
