ee33dfd0025a80d16116b3e4409c3df724df6d33
Carlos Velasco <carlos@2activedesign.com>
2017-06-06 23:23:13 +0200

    bankfuptcy possible, simplified layout

--- js/master.js
+++ js/master.js
@@ -5,6 +5,8 @@ window.lemonade = {
   allTimeProfit: 10,
   dailyProfit: 0,
   bar: document.getElementById('percentageBar'),
+  play: document.getElementById('play'),
+
   randomNumber: function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
@@ -14,6 +16,21 @@ window.lemonade = {
   todayWeatherVariant: 3,
   todayForecast: '',
   emotionBank: ['mdi mdi-emoticon-dead mdi-36px', 'mdi mdi-emoticon-neutral mdi-36px', 'mdi mdi-emoticon mdi-36px', 'mdi mdi-emoticon-excited mdi-36px'],
+  firstDay: function firstDay() {
+    lemonade.day = 0;
+    lemonade.tomorrowWeatherVariant = 3;
+    lemonade.todayWeatherVariant = 3;
+    lemonade.sold = 0;
+    lemonade.allTimeProfit = 10;
+    lemonade.dailyProfit = 0;
+    lemonade.emotion.className = lemonade.emotionBank[3];
+    lemonade.play.removeEventListener('click', lemonade.firstDay);
+    lemonade.play.addEventListener('click', lemonade.nextDay);
+    lemonade.displayUpdate();
+  },
+  emotion: document.getElementById('emotionDisplay'),
+  sold: 0,
+  forecastDisplay: document.getElementById('forecastDisplay'),
   nextDay: function nextDay() {
     const cups = document.getElementById('cups')
       .value;
@@ -21,59 +38,58 @@ window.lemonade = {
       .value;
     const signs = document.getElementById('signs')
       .value;
-    const forecastDisplay = document.getElementById('forecastDisplay');
-    const emotion = document.getElementById('emotionDisplay');
-    if (lemonade.day < 2) {
-      lemonade.todayWeatherVariant = 3;
-      lemonade.todayForecast = 'Hot/Dry';
-    } else {
-      lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
-      lemonade.todayForecast = lemonade.tomorrowForecast;
-    }
+
+
+    lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
+    lemonade.todayForecast = lemonade.tomorrowForecast;
+
     const marketingResult = (((signs * 1.35) + (lemonade.randomNumber(14.4, 94.2) / 70)) / price) * ((lemonade.todayWeatherVariant / 2.5) + 0.08);
-    let sold = Math.round(marketingResult);
-    if (marketingResult > cups) { sold = cups; }
-    if (sold > 0) {
-      lemonade.bar.style.width = ((sold / cups) * 100) + '%';
+    lemonade.sold = Math.round(marketingResult);
+    if (marketingResult > cups) { lemonade.sold = cups; }
+    if (lemonade.sold > 0) {
+      lemonade.bar.style.width = ((lemonade.sold / cups) * 100) + '%';
     } else {
       lemonade.bar.style.width = 0;
     }
-    const profits = Math.round((sold * price) * 100) / 100;
+    const profits = Math.round((lemonade.sold * price) * 100) / 100;
     const expenses = Math.round(((signs * lemonade.signCost) + (cups * lemonade.cupCost)) * 100) / 100;
     lemonade.dailyProfit = Math.round((profits - expenses) * 100) / 100;
     lemonade.allTimeProfit += lemonade.dailyProfit;
     lemonade.allTimeProfit = Math.round(lemonade.allTimeProfit * 100) / 100;
     Materialize.toast('Profit: $' + profits + ' | Expense: $' + expenses + ' | Net: $' + lemonade.dailyProfit, 6000);
     if (lemonade.allTimeProfit < 0.01) {
-      emotion.className = lemonade.emotionBank[0];
+      lemonade.emotion.className = lemonade.emotionBank[0];
       Materialize.toast('Bankrupt!', 6000);
+      lemonade.play.removeEventListener('click', lemonade.nextDay);
+      lemonade.play.addEventListener('click', lemonade.firstDay);
     } else if (lemonade.allTimeProfit < 12) {
-      emotion.className = lemonade.emotionBank[1];
+      lemonade.emotion.className = lemonade.emotionBank[1];
     } else if (lemonade.allTimeProfit < 34) {
-      emotion.className = lemonade.emotionBank[2];
+      lemonade.emotion.className = lemonade.emotionBank[2];
     } else if (lemonade.allTimeProfit > 74) {
-      emotion.className = lemonade.emotionBank[3];
+      lemonade.emotion.className = lemonade.emotionBank[3];
     }
 
     lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 4);
     // lemonade.tomorrowWeatherVariant = 1;
-    lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
-    forecastDisplay.className = lemonade.tomorrowForecast;
 
     lemonade.day++;
+    lemonade.displayUpdate();
+  },
+  displayUpdate: function displayUpdate() {
+    lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
+    lemonade.forecastDisplay.className = lemonade.tomorrowForecast;
+
     document.getElementById('dayDisplay')
       .innerText = 'Day ' + lemonade.day;
     document.getElementById('cupsSoldDisplay')
-      .innerText = 'Cups Sold Today: ' + sold;
+      .innerText = 'Cups Sold Today: ' + lemonade.sold;
     document.getElementById('grandTotalDisplay')
       .innerText = '$' + lemonade.allTimeProfit;
-  },
+  }
 };
-const play = document.getElementById('play');
-
-play.addEventListener('click', lemonade.nextDay);
+lemonade.play.addEventListener('click', lemonade.nextDay);
 
 window.onload = function () {
   play.className += ' scale-in';
-  Materialize.fadeInImage('#lemonadeImage');
 };
