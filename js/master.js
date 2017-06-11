2411238e34875f2047cb9a4574bd65202bb1a7e1
Carlos Velasco <carlos@2activedesign.com>
2017-06-11 17:17:10 +0200

    number adjustment and maintainability update

--- js/master.js
+++ js/master.js
@@ -14,55 +14,89 @@ window.lemonade = {
   forecastDisplay: document.getElementById('forecastDisplay'),
   bar: document.getElementById('percentageBar'),
   play: document.getElementById('play'),
-  weather: ['mdi mdi-weather-lightning-rainy mdi-36px', 'mdi mdi-weather-cloudy mdi-36px',
-    'mdi mdi-weather-partlycloudy mdi-36px', 'mdi mdi-weather-sunny mdi-36px',
+  cupsVar: document.getElementById('cups'),
+  signsVar: document.getElementById('signs'),
+  priceVar: document.getElementById('price'),
+  strawVar: document.getElementById('straw'),
+  lemonVar: document.getElementById('lemon'),
+  glassTopVar: document.getElementById('glassTop'),
+  glassBottom: document.getElementById('glassBottom'),
+  lemonade1: document.getElementById('lemonade1'),
+  lemonade2: document.getElementById('lemonade2'),
+  dayDisplayVar: document.getElementById('dayDisplay'),
+  cupsSoldDisplayVar: document.getElementById('cupsSoldDisplay'),
+  grandTotalDisplayVar: document.getElementById('grandTotalDisplay'),
+  weather: ['mdi mdi-weather-lightning-rainy mdi-36px',
+    'mdi mdi-weather-cloudy mdi-36px',
+    'mdi mdi-weather-partlycloudy mdi-36px',
+    'mdi mdi-weather-sunny mdi-36px',
   ],
-  emotionBank: ['mdi mdi-emoticon-dead mdi-36px', 'mdi mdi-emoticon-sad mdi-36px',
-    'mdi mdi-emoticon-neutral mdi-36px', 'mdi mdi-emoticon mdi-36px', 'mdi mdi-emoticon-excited mdi-36px',
+  emotionBank: ['mdi mdi-emoticon-dead mdi-36px',
+    'mdi mdi-emoticon-sad mdi-36px',
+    'mdi mdi-emoticon-neutral mdi-36px', 'mdi mdi-emoticon mdi-36px',
+    'mdi mdi-emoticon-excited mdi-36px',
     'mdi mdi-emoticon-cool mdi-36px',
   ],
+  diagnostic: (signs) => {
+    console.debug(
+      `${lemonade.signsVar.valueAsNumber
+      } signs market: ${lemonade.stringRound(signs)
+      } Cost: $${lemonade.stringRound(lemonade.priceVar.valueAsNumber)
+      } Weather ${lemonade.todayWeatherVariant
+      } Sold ${lemonade.sold} cups`,
+    );
+  },
   whenLoaded: () => {
-    // lemonade.cheat(); // Enable cheating
+    lemonade.pourIn('');
     lemonade.play.addEventListener('click', lemonade.nextDay);
     lemonade.displayUpdate();
     document.getElementById('loader')
       .remove();
     document.getElementById('play')
       .className += ' scale-in';
-    lemonade.pourIn('');
     Materialize.toast("It's a nice day to sell Lemonade!", 5000);
+    // lemonade.cheat(); // Enable cheating
   },
   cheat: () => {
     lemonade.cheating = true;
-    lemonade.day = 1000;
-    lemonade.allTimeProfit = 2000;
+    Materialize.toast('Cheat enabled', 4000);
+    lemonade.day = 40000;
+    lemonade.allTimeProfit = 40000;
     lemonade.confidence = 3;
     lemonade.tomorrowForecast = lemonade.cheatWeather;
-    document.getElementById('cups')
-      .valueAsNumber = 150;
-    document.getElementById('signs')
-      .valueAsNumber = 20;
-    document.getElementById('price')
-      .valueAsNumber = 1;
-    lemonade.cheatWeather = 1;
+    lemonade.cupsVar.valueAsNumber = 300;
+    lemonade.signsVar.valueAsNumber = 10;
+    lemonade.priceVar.valueAsNumber = 0.50;
+    lemonade.cheatWeather = 2;
+    // lemonade.randomNumber = () => 3;
     lemonade.displayUpdate();
-    lemonade.nextDay();
-    lemonade.nextDay();
-    lemonade.nextDay();
-    Materialize.toast('Cheat enabled', 4000);
+    for (let i = 14; i > 0; i -= 1) {
+      // lemonade.cupsVar.valueAsNumber += 2;
+      lemonade.priceVar.valueAsNumber += 0.50;
+      // lemonade.cheatWeather -= 1;
+      lemonade.nextDay();
+    }
     return 'You are cheating!';
   },
-  randomNumber: (min, max) => Math.floor(Math.random() * (max - min)) + min,
-  stringRound: equation => equation.toFixed(2),
-  twoDecimals: equation => Math.round(equation * 100) / 100,
   marketingResult: () => {
-    const signs = lemonade.signs ** 5;
-    const confidence = lemonade.randomNumber(1, 3) * lemonade.confidence;
-    const price = (lemonade.price) ** 6;
-    const weather = lemonade.todayWeatherVariant ** 2;
+    let signs = (
+      ((lemonade.signs) ** 2) / Math.log1p(lemonade.signs));
+    if (isNaN(signs) || signs < 1) signs = 0;
+    if (lemonade.cheating) lemonade.diagnostic(signs);
+    const confidence = (lemonade.randomNumber(1.1, 4.5) * lemonade.confidence);
+    const price = (lemonade.price);
+    const weather = (lemonade.todayWeatherVariant ** 2) + 1;
     const result = ((signs + confidence) / price) * weather;
     return result + 0.08;
   },
+  toastVar: () => document.getElementById('toast-container')
+    .innerHTML,
+  randomNumber: (min, max) => Math.floor(Math.random() * (max - min)) +
+    min,
+  stringRound: equation => equation.toFixed(2),
+  twoDecimals: equation => Math.round(equation * 100) / 100,
+  average: () => ((lemonade.allTimeProfit - lemonade.dailyProfit) /
+    lemonade.day),
   determineConfidence: () => {
     if (lemonade.allTimeProfit < 10) {
       lemonade.confidence = 0;
@@ -78,7 +112,8 @@ window.lemonade = {
       lemonade.confidence = 1;
       // console.log('option 1 ' + lemonade.confidence);
     } else if (lemonade.dailyProfit - lemonade.average() < 60) {
-      lemonade.confidence = Math.round(((lemonade.dailyProfit - lemonade.average()) / 20) + 2);
+      lemonade.confidence = Math.round(((lemonade.dailyProfit -
+        lemonade.average()) / 20) + 2);
       // console.log('option 2 ' + lemonade.confidence);
     } else {
       lemonade.confidence = 5;
@@ -86,9 +121,90 @@ window.lemonade = {
     }
     return lemonade.confidence;
   },
+  newGame: () => {
+    lemonade.play.removeEventListener('click', lemonade.newGame);
+    lemonade.day = 0;
+    lemonade.tomorrowWeatherVariant = 3;
+    lemonade.todayWeatherVariant = 3;
+    lemonade.sold = 0;
+    lemonade.allTimeProfit = 20;
+    lemonade.dailyProfit = 0;
+    lemonade.emotion.className = lemonade.emotionBank[3];
+    lemonade.emotion.className = lemonade.emotionBank[3];
+    lemonade.confidence = 3;
+    lemonade.cupsVar.valueAsNumber = 150;
+    lemonade.signsVar.valueAsNumber = 10;
+    lemonade.priceVar.valueAsNumber = 4;
+    lemonade.clearToast();
+    Materialize.toast("It's a nice day to sell Lemonade!", 5000);
+    lemonade.displayUpdate();
+    lemonade.pourIn();
+    lemonade.play.addEventListener('click', lemonade.nextDay);
+  },
+  nextDay: () => {
+    lemonade.cups = lemonade.cupsVar.valueAsNumber;
+    lemonade.price = lemonade.priceVar.valueAsNumber;
+    lemonade.signs = lemonade.signsVar.valueAsNumber;
+    lemonade.clean();
+    lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
+    lemonade.todayForecast = lemonade.tomorrowForecast;
+    lemonade.expenses = lemonade.twoDecimals(
+      (lemonade.signs * lemonade.signCost) + (lemonade.cups *
+        lemonade.cupCost));
+    if (lemonade.expenses > lemonade.allTimeProfit) {
+      lemonade.clearToast();
+      Materialize.toast(
+        `You can't afford $${lemonade.stringRound(lemonade.expenses)}!`,
+        2000);
+      Materialize.toast(
+        `$${lemonade.stringRound(lemonade.cupCost)
+        } per Cup | $${lemonade.stringRound(lemonade.signCost)} per Sign`,
+        6000);
+      lemonade.cupsVar.valueAsNumber = lemonade.allTimeProfit - 2;
+      lemonade.signsVar.valueAsNumber = 2;
+    } else {
+      lemonade.sold = Math.round(lemonade.marketingResult());
+      if (lemonade.sold > 0 && lemonade.sold < lemonade.cups) {
+        lemonade.bar.style.width =
+          `${(lemonade.sold / lemonade.cups) * 100}%`;
+      } else if (lemonade.sold === 0) {
+        lemonade.bar.style.width = 0;
+      } else {
+        lemonade.sold = lemonade.cups;
+        lemonade.bar.style.width =
+          `${(lemonade.sold / lemonade.cups) * 100}%`;
+      }
+      lemonade.profits = lemonade.twoDecimals(lemonade.sold *
+        lemonade.price);
+      lemonade.dailyProfit = lemonade.twoDecimals(lemonade.profits -
+        lemonade.expenses);
+      lemonade.allTimeProfit += lemonade.dailyProfit;
+      lemonade.allTimeProfit = lemonade.twoDecimals(lemonade.allTimeProfit);
+      Materialize.toast(
+        `Profit: $${lemonade.stringRound(lemonade.profits)
+        } | Expense: $${lemonade.stringRound(lemonade.expenses)}`,
+        6000);
+      lemonade.determineConfidence();
+      lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 4);
+      if (lemonade.cheating) {
+        lemonade.tomorrowWeatherVariant = lemonade.cheatWeather;
+      }
+      lemonade.day += 1;
+      lemonade.displayUpdate();
+    }
+  },
+  displayUpdate: () => {
+    lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
+    lemonade.forecastDisplay.className = lemonade.tomorrowForecast;
+    lemonade.emotion.className = lemonade.emotionBank[lemonade.confidence];
+    lemonade.dayDisplayVar.innerText = `Day ${lemonade.day}`;
+    lemonade.cupsSoldDisplayVar.innerText =
+      `${lemonade.sold} Cups | $${lemonade.stringRound(lemonade.dailyProfit)}`;
+    lemonade.grandTotalDisplayVar.innerText =
+      `$${lemonade.stringRound(lemonade.allTimeProfit)}`;
+  },
   clearToast: () => {
-    document.getElementById('toast-container')
-      .innerHTML = '';
+    lemonade.toastVar = '';
   },
   clean: () => {
     if (document.getElementById('range1')
@@ -108,10 +224,8 @@ window.lemonade = {
     }
   },
   resetAnimation: () => {
-    document.getElementById('straw')
-      .classList.remove('straw2');
-    document.getElementById('lemon')
-      .classList.remove('lemon2');
+    lemonade.strawVar.classList.remove('straw2');
+    lemonade.lemonVar.classList.remove('lemon2');
     document.getElementById('lemonade1')
       .classList.remove('liquid2');
     document.getElementById('lemonade2')
@@ -120,14 +234,10 @@ window.lemonade = {
       document.getElementById(`cube${i}`)
         .classList.remove('cubes2');
     }
-    document.getElementById('straw')
-      .classList.remove('straw');
-    document.getElementById('lemon')
-      .classList.remove('lemon');
-    document.getElementById('glassTop')
-      .classList.remove('glass');
-    document.getElementById('glassBottom')
-      .classList.remove('glass');
+    lemonade.strawVar.classList.remove('straw');
+    lemonade.lemonVar.classList.remove('lemon');
+    lemonade.glassTopVar.classList.remove('glass');
+    lemonade.glassBottom.classList.remove('glass');
     document.getElementById('lemonade1')
       .classList.remove('liquid');
     document.getElementById('lemonade2')
@@ -140,10 +250,8 @@ window.lemonade = {
   pourIn: () => {
     lemonade.resetAnimation();
     setTimeout(() => {
-      document.getElementById('straw')
-        .classList.add('straw');
-      document.getElementById('lemon')
-        .classList.add('lemon');
+      lemonade.strawVar.classList.add('straw');
+      lemonade.lemonVar.classList.add('lemon');
       document.getElementById('lemonade1')
         .classList.add('liquid');
       document.getElementById('lemonade2')
@@ -157,100 +265,17 @@ window.lemonade = {
   pourOut: () => {
     lemonade.resetAnimation();
     setTimeout(() => {
-      document.getElementById('straw')
-        .classList.add('straw2');
-      document.getElementById('lemon')
-        .classList.add('lemon2');
-      document.getElementById('glassTop')
-        .classList.add('glass2');
-      document.getElementById('glassBottom')
-        .classList.add('glass2');
-      document.getElementById('lemonade1')
-        .classList.add('liquid2');
-      document.getElementById('lemonade2')
-        .classList.add('liquid2');
+      lemonade.strawVar.classList.add('straw2');
+      lemonade.lemonVar.classList.add('lemon2');
+      lemonade.glassTopVar.classList.add('glass2');
+      lemonade.glassBottom.classList.add('glass2');
+      lemonade.lemonade1.classList.add('liquid2');
+      lemonade.lemonade2.classList.add('liquid2');
       for (let cubesIn = 1; cubesIn < 6; cubesIn += 1) {
         document.getElementById(`cube${cubesIn}`)
           .classList.add('cubes2');
       }
     }, 5);
-  },
-  newGame: () => {
-    lemonade.clearToast();
-    lemonade.day = 0;
-    lemonade.tomorrowWeatherVariant = 3;
-    lemonade.todayWeatherVariant = 3;
-    lemonade.sold = 0;
-    lemonade.allTimeProfit = 20;
-    lemonade.dailyProfit = 0;
-    lemonade.emotion.className = lemonade.emotionBank[3];
-    lemonade.play.removeEventListener('click', lemonade.newGame);
-    lemonade.play.addEventListener('click', lemonade.nextDay);
-    lemonade.emotion.className = lemonade.emotionBank[3];
-    lemonade.confidence = 3;
-    document.getElementById('cups')
-      .valueAsNumber = 75;
-    document.getElementById('signs')
-      .valueAsNumber = 10;
-    document.getElementById('price')
-      .valueAsNumber = 4;
-    lemonade.displayUpdate();
-    lemonade.pourIn();
-    Materialize.toast("It's a nice day to sell Lemonade!", 5000);
-  },
-  average: () => ((lemonade.allTimeProfit - lemonade.dailyProfit) / lemonade.day),
-  nextDay: () => {
-    lemonade.cups = document.getElementById('cups')
-      .valueAsNumber;
-    lemonade.price = document.getElementById('price')
-      .valueAsNumber;
-    lemonade.signs = document.getElementById('signs')
-      .valueAsNumber;
-    lemonade.clean();
-    lemonade.todayWeatherVariant = lemonade.tomorrowWeatherVariant;
-    lemonade.todayForecast = lemonade.tomorrowForecast;
-    lemonade.expenses = lemonade.twoDecimals(
-      (lemonade.signs * lemonade.signCost) + (lemonade.cups * lemonade.cupCost));
-    if (lemonade.expenses > lemonade.allTimeProfit) {
-      lemonade.clearToast();
-      Materialize.toast(`You can't afford $${lemonade.stringRound(lemonade.expenses)}!`, 2000);
-      Materialize.toast(`$${lemonade.stringRound(lemonade.cupCost)} per Cup | $${lemonade.stringRound(lemonade.signCost)} per Sign`, 6000);
-      document.getElementById('cups')
-        .valueAsNumber = lemonade.allTimeProfit - 2;
-      document.getElementById('signs')
-        .valueAsNumber = 2;
-    } else {
-      lemonade.sold = Math.round(lemonade.marketingResult());
-      if (lemonade.marketingResult() > lemonade.cups) { lemonade.sold = lemonade.cups; }
-      if (lemonade.sold > 0) {
-        lemonade.bar.style.width = `${(lemonade.sold / lemonade.cups) * 100}%`;
-      } else {
-        lemonade.bar.style.width = 0;
-      }
-      lemonade.profits = lemonade.twoDecimals(lemonade.sold * lemonade.price);
-      lemonade.dailyProfit = lemonade.twoDecimals(lemonade.profits - lemonade.expenses);
-      lemonade.allTimeProfit += lemonade.dailyProfit;
-      lemonade.allTimeProfit = lemonade.twoDecimals(lemonade.allTimeProfit);
-      Materialize.toast(
-        `Profit: $${lemonade.stringRound(lemonade.profits)} | Expense: $${lemonade.stringRound(lemonade.expenses)}`,
-        3000);
-      lemonade.determineConfidence();
-      lemonade.tomorrowWeatherVariant = lemonade.randomNumber(0, 4);
-      if (lemonade.cheating) { lemonade.tomorrowWeatherVariant = lemonade.cheatWeather; }
-      lemonade.day += 1;
-      lemonade.displayUpdate();
-    }
-  },
-  displayUpdate: () => {
-    lemonade.tomorrowForecast = lemonade.weather[lemonade.tomorrowWeatherVariant];
-    lemonade.forecastDisplay.className = lemonade.tomorrowForecast;
-    lemonade.emotion.className = lemonade.emotionBank[lemonade.confidence];
-    document.getElementById('dayDisplay')
-      .innerText = `Day ${lemonade.day}`;
-    document.getElementById('cupsSoldDisplay')
-      .innerText = `${lemonade.sold} Cups | $${lemonade.stringRound(lemonade.dailyProfit)}`;
-    document.getElementById('grandTotalDisplay')
-      .innerText = `$${lemonade.stringRound(lemonade.allTimeProfit)}`;
   },
 };
 window.onload = () => {
