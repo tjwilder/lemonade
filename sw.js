6ff2f0ae78de8313ee5c14f6a92853d84b669e9b
Carlos Velasco <carlos@2activedesign.com>
2017-06-22 17:27:42 +0200

    offline support

--- sw.js
+++ sw.js
@@ -57,13 +57,13 @@ var __wpo = {
     "cb8f61464f349af887ba96f6a2ae1e318b3cc8d9": "./fonts/roboto/Roboto-Thin.woff",
     "c321aeb611cf1dcb25717484a4ced717d8ca76fa": "./fonts/roboto/Roboto-Thin.woff2",
     "a81fdb60859f9ae223da7ecc4544416136671eac": "./embedEn.html",
-    "be19b30f02d6a30875dbc8cd66f9bb28328fd300": "./",
+    "4f9e6a79f51ffc79fbf9321bed0d6aa929bec428": "./",
     "3bfd63c8b50e0416b11cb5c050ab94701270d2db": "./bundle.js",
     "91c0bb4f40fd78cdf16036a5eaaded081ad70cec": "./main.css"
   },
   "strategy": "all",
   "responseStrategy": "network-first",
-  "version": "2017-6-22 17:09:20",
+  "version": "2017-6-22 17:26:07",
   "name": "webpack-offline",
   "pluginVersion": "4.8.1",
   "relativePaths": true
